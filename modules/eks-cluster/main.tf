b0821581e9534fae51fcd0906628222875946416
mrswalter404 <mrswalter404@yahoo.com>
2025-09-02 22:26:23 -0500

    corrected oidc provider

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 20937684d3d60de2efef84efc024d22cf01aeda6
 ##############################################
 # EKS Data + Auth
 ##############################################
@@ -21,6 +17,30 @@ provider "kubernetes" {
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
+
+
+# data "aws_eks_cluster" "this" {
+#   name = var.cluster_name
+# }
+
+# Get EKS cluster info
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+# Get OIDC provider automatically
+data "aws_iam_openid_connect_provider" "oidc" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+# Get OIDC provider ARN
+# data "aws_eks_cluster_auth" "this" {
+#   name = var.cluster_name
+# }
+
+# data "aws_iam_openid_connect_provider" "oidc" {
+#   arn = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+# }
+
 
 ##############################################
 # EKS Control Plane + Node Groups + Add-ons
@@ -69,11 +89,7 @@ module "eks" {
     vpc-cni = {
       most_recent              = true
       service_account_role_arn = var.cni_role_arn
-<<<<<<< HEAD
       resolve_conflicts        = "OVERWRITE"
-=======
-      resolve_conflicts = "OVERWRITE"
->>>>>>> 20937684d3d60de2efef84efc024d22cf01aeda6
     }
 
     eks-pod-identity-agent = {
@@ -123,11 +139,7 @@ module "eks" {
 
       policy_associations = [
         {
-<<<<<<< HEAD
           policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-=======
-          policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
->>>>>>> 20937684d3d60de2efef84efc024d22cf01aeda6
           access_scope = { type = "cluster" }
         }
       ]
@@ -139,11 +151,7 @@ module "eks" {
 
       policy_associations = [
         {
-<<<<<<< HEAD
           policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-=======
-          policy_arn  = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
->>>>>>> 20937684d3d60de2efef84efc024d22cf01aeda6
           access_scope = { type = "cluster" }
         }
       ]
@@ -241,173 +249,4 @@ resource "kubernetes_namespace" "fintech_dev" {
   }
 
   depends_on = [module.eks]
-<<<<<<< HEAD
 }
-
-
-# data "aws_eks_cluster_auth" "main" {
-#   name = var.cluster_name
-# }
-
-# data "aws_caller_identity" "current" {}
-
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.main.token
-#   alias                  = "eks"
-# }
-
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 20.0"
-
-#   cluster_name    = var.cluster_name
-#   cluster_version = "1.32"
-
-#   enable_cluster_creator_admin_permissions = true
-#   cluster_endpoint_public_access           = true
-#   bootstrap_self_managed_addons            = false
-
-#   vpc_id                                = var.vpc_id
-#   subnet_ids                            = var.private_subnets
-#   control_plane_subnet_ids              = var.private_subnets
-#   cluster_additional_security_group_ids = var.security_group_ids
-
-#   create_cloudwatch_log_group = true
-#   cluster_enabled_log_types   = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-
-#   cluster_addons = {
-#     coredns = {
-#       most_recent = true
-#     }
-#     kube-proxy = {
-#       most_recent = true
-#     }
-#     vpc-cni = {
-#       most_recent              = true
-#       service_account_role_arn = var.cni_role_arn
-#     }
-#     eks-pod-identity-agent = {
-#       most_recent = true
-#     }
-#   }
-
-#   eks_managed_node_group_defaults = {
-#     ami_type       = "AL2023_x86_64_STANDARD"
-#     instance_types = ["t2.medium"]
-#     min_size       = 1
-#     max_size       = 10
-#     desired_size   = 1
-#     iam_role_additional_policies = {
-#       AmazonEKS_CNI_Policy = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#     }
-#   }
-
-#   eks_managed_node_groups = {
-#     eks-node-group-2 = {
-#       # Uses the defaults above
-#     }
-#   }
-
-#   access_entries = {
-#     # ✅ Map 'fusi' user to eks-admins
-#     azwe = {
-#       kubernetes_groups = ["eks-admins"]
-#       principal_arn     = "arn:aws:iam::514670561567:user/azwe"
-
-#       policy_associations = [
-#         {
-#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-#           access_scope = {
-#             type = "cluster"
-#           }
-#         }
-#       ]
-#     }
-
-#     github_runner = {
-#       kubernetes_groups = ["eks-admins"]
-#       principal_arn     = "arn:aws:iam::514670561567:role/github-runner-ssm-role"
-
-#       policy_associations = [
-#         {
-#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-#           access_scope = {
-#             type = "cluster"
-#           }
-#         }
-#       ]
-#     }
-#   }
-
-#   tags = local.common_tags
-# }
-
-# # -----------------------------------------
-# # ✅ ClusterRoleBinding for eks-admins group
-# # -----------------------------------------
-# resource "kubernetes_cluster_role_binding" "eks_admins_binding" {
-#   provider = kubernetes.eks
-
-#   metadata {
-#     name = "eks-admins-binding"
-#   }
-
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
-
-#   subject {
-#     kind      = "Group"
-#     name      = "eks-admins"
-#     api_group = "rbac.authorization.k8s.io"
-#   }
-# }
-
-# ################################################################################
-# # Kubernetes Namespaces
-# ################################################################################
-
-# resource "kubernetes_namespace" "fintech" {
-#   metadata {
-#     name = "fintech"
-#     annotations = {
-#       name = "fintech"
-#     }
-#     labels = {
-#       app = "fintech"
-#     }
-#   }
-# }
-
-# resource "kubernetes_namespace" "monitoring" {
-#   metadata {
-#     name = "monitoring"
-#     annotations = {
-#       name = "monitoring"
-#     }
-#     labels = {
-#       app = "monitoring"
-#     }
-#   }
-# }
-
-# resource "kubernetes_namespace" "fintech_dev" {
-#   metadata {
-#     name = "fintech-dev"
-#     annotations = {
-#       name = "fintech-dev"
-#     }
-#     labels = {
-#       app = "fintech-dev"
-#     }
-#   }
-# }
-
-
-=======
-}
->>>>>>> 20937684d3d60de2efef84efc024d22cf01aeda6
